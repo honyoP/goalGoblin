be237739ff8e9695e03c2e54f6240cc5d8f67368
Tadeas Tran <tran.tadeas@protonmail.com>
2025-01-17 22:12:28 +0100

    Setup storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Storage } from './storage'
+import { Character } from './Character'
 
 function App() {
+  const [user, setUser] = useState<Character | null>(null);
   const [count, setCount] = useState(0)
+
+  const character: Character = {
+    dexterity: 12,
+    experience: 0,
+    health: 15,
+    intelligence: 10,
+    level: 1,
+    mana: 15,
+    name: "Tadeas",
+    shekels: 0,
+    skill_points: 0,
+    stamina: 15,
+    strength: 15,
+    total_items_earned: 0,
+    total_monsters_slain: 0,
+    total_quests_done: 0,
+    total_shekels_earned: 0,
+    total_time_spent: "",
+    vitality: 10,
+  }
+
+  useEffect(() => {
+    if(user)
+    {
+      handleUpload(user);
+    } else {
+      handleUpload(character)
+      loadCharacter();
+    }
+  }, [])
+
+  const handleUpload = async (character: Character) => {
+    if (character) {
+      try {
+        await Storage.set('GoalGoblin_userCharacter', character);
+      } catch (error) {
+        console.error('Invalid JSON file:', error);
+      }
+    }
+  }
+
+  const loadCharacter = async () => {
+    const storedCharacter = await Storage.get<Character>('GoalGoblin_userCharacter');
+    setUser(storedCharacter || null);
+  }
 
   return (
     <>
@@ -22,7 +70,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {user?.name}
         </p>
       </div>
       <p className="read-the-docs">
---
+++ src/storage.ts
@@ -0,0 +1,38 @@
+type StorageKey = 'GoalGoblin_userCharacter';
+
+export const Storage = {
+    set: <T>(key: StorageKey, value: T): Promise<void> => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.set({ [key]: value }, () => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve();
+                }
+            })
+        });
+    },
+    get: <T>(key: StorageKey): Promise<T | undefined> => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get([key], (result) => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve(result[key] as T);
+                }
+            });
+        });
+    },
+
+    remove: (key: StorageKey): Promise<void> => {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.remove([key], () => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+}
