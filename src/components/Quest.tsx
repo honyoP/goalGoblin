6bd2fd744dcc6630872401f0f7c6f115654de989
MartinLetocha <martin.letocha2@gmail.com>
2025-01-18 00:11:21 +0100

    busted rn

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -1,5 +1,21 @@
-export default function Quest({which}:Props) {
-    return <button onClick={()=>{alert(which)}}>{GetRandomName()} ({GiveTime(which)})</button>
+export default function Quest({which, actives, setActive}:Props) {
+    if(actives[which-1] === 0)
+    {
+        currentStyle = disabledStyle;
+    }
+    else{
+        currentStyle = activeStyle;
+    }
+    const handleClick = () => {
+        console.log("i'm fucking alive");
+        setActive(
+            actives.map((value, index) => 
+                index === which - 1 ? 1 : value
+            )
+        );
+    };
+    return <button style={currentStyle} onClick={()=>handleClick()}>
+        {GetRandomName()} ({GiveTime(which)})</button>
 };
 
 function GiveTime(which:number)
@@ -12,8 +28,21 @@ function GiveTime(which:number)
 }
 
 type Props = {
-    which: number;
+    which: number,
+    actives: number[];
+    setActive: React.Dispatch<React.SetStateAction<number[]>>;
 };
+
+const activeStyle = {
+    backgroundColor: "white",
+    color: "black"
+}
+
+const disabledStyle = {
+    backgroundColor: "black",
+    color: "white"
+}
+let currentStyle = disabledStyle;
 
 function GetRandomName(): string {
     let max = names.length;
--- src/components/QuestWindow.tsx
+++ src/components/QuestWindow.tsx
@@ -1,21 +1,23 @@
 import Quest from './Quest';
-
+import {useState} from 'react';
 const containerStyle: React.CSSProperties = {
     display: "flex",
     flexDirection: "column",
     gap: "10px",
 };
 
+
 export default function QuestWindow() {
-    return <div style={containerStyle}>{GetQuests(5)}</div>;
+    const [actives, setActive] = useState<number[]>([0,0,0,0,0]);
+    return <div style={containerStyle}>{GetQuests(5, actives, setActive)}</div>;
 }
 
-function GetQuests(n:number)
+function GetQuests(n:number, actives:number[], setActive:React.Dispatch<React.SetStateAction<number[]>>)
 {
     const result = [];
     for(let i = 0; i < n; i++)
     {
-        result.push(<Quest which = {i + 1}/>);
+        result.push(<Quest which = {i + 1} actives={actives} setActive={setActive}/>);
     }
     return result;
 }
